8d9ac67000eff6d1028947e185b5666244653a9f
binam1997 <126240906+binam1997@users.noreply.github.com>
2026-08-21 12:22:18 +0330

    Change timezone from UTC to Asia/Tehran

--- dmi_alert.py
+++ dmi_alert.py
@@ -86,7 +86,7 @@ def get_data():
         "interval": INTERVAL,
         "outputsize": OUTPUT_SIZE,
         "apikey": TWELVEDATA_API_KEY,
-        "timezone": "UTC"
+        "timezone": "Asia/Tehran"
     }
 
     response = requests.get(
